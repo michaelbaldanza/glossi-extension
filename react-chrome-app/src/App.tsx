a7eba6e4855858dfe1ef976023ea82565f084767
Michael Baldanza <michael.snider.baldanza@gmail.com>
2023-09-13 16:26:46 -0400

    change extension from a popup to a sidepanel. add service-worker.js to add the extension to the context menu and pass selections to the React app. internationalize the extension: set manifest's default_locale key to en, add _locales/en directories, set programmatic values for manifest.json name and description keys in _locales/en/messages.json

--- react-chrome-app/src/App.tsx
+++ react-chrome-app/src/App.tsx
@@ -5,6 +5,7 @@ import { User } from './services/interfaces';
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [lookup, setLookup] = useState<string>('');
 
   useEffect(() => {
     chrome.storage.local.get(['user']).then((result) => {
@@ -12,6 +13,17 @@ function App() {
         setUser(result.user);
       }
     });
+
+    chrome.runtime.onMessage.addListener(({ name, data }) => {
+      /*
+        Adapted from Google Chrome's Dictionary side panel example.
+        https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/functional-samples/sample.sidepanel-dictionary/sidepanel.js
+      */
+      if (name === 'glossi-define') {
+        setLookup(data.value);
+      }
+    });
+
   }, [])
 
   return (
@@ -25,6 +37,13 @@ function App() {
           </div>
           :
           <Login user={[user, setUser]} />
+        }
+      </div>
+      <div>
+        {
+          lookup.length ?
+          'Going to look up ' + lookup :
+          'Right click on a word and select the Glossi option to get started.'
         }
       </div>
     </div>
